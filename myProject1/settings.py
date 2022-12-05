865f12cc107f735e81b5ec014597a958f3a54631
playfulPangolin <valiban12@gmail.com>
2022-12-05 22:51:38 +0800

    Remove local host in AllowedHosts

--- myProject1/settings.py
+++ myProject1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-xn+c=nyl!&dl07#yum8%tctrc9gk_5h79@%j2!qe-o@duqw+wt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 

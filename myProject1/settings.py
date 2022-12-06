e07ac6b02112d4e84a90aa1419df8ce3394b72de
playfulPangolin <valiban12@gmail.com>
2022-12-06 14:36:16 +0800

    Changed debug to False

--- myProject1/settings.py
+++ myProject1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-xn+c=nyl!&dl07#yum8%tctrc9gk_5h79@%j2!qe-o@duqw+wt
 # )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["*"]
